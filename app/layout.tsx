b2067bbb7b0a6ad452442639d5e745f50a492bec
Omofovwe Akpadaka <afovwe@gmail.com>
2024-07-29 22:50:41 +0100

    clerk and convex linked not tested yet

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import  ConvexClerkProvider  from "./providers/ConvexClerkProvider";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+       
+         <ConvexClerkProvider>{children}</ConvexClerkProvider>
+        </body>
     </html>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+//https://helped-troll-62.clerk.accounts.dev
+const authConfig = {
+  providers: [
+    {
+      domain: "https://helped-troll-62.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+
+export default authConfig;
